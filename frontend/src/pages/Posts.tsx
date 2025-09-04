fdcdcf9c245fe57720363bdcdae99aa8859820e5
Victor Aiyeola <aiyeolavictor@gmail.com>
2025-09-04 07:49:37 +0100

    feat: update Posts component layout for responsive grid display

--- frontend/src/pages/Posts.tsx
+++ frontend/src/pages/Posts.tsx
@@ -48,7 +48,7 @@ export default function Posts() {
           <p>{posts?.length} Posts</p>
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-4 mt-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-6">
         <AddNewPost />
         {posts?.map((post: Record<string, string>) => (
           <div
